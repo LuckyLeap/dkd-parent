4c32db57908bbf35d133ac46336aa7b416678274
LuckyLeap <2754984034@qq.com>
2025-04-04 16:01:48 +0800

    帝可得项目管理

---
+++ dkd-manage/src/main/java/com/dkd/manage/mapper/SkuClassMapper.java
@@ -0,0 +1,58 @@
+package com.dkd.manage.mapper;
+
+import java.util.List;
+import com.dkd.manage.domain.SkuClass;
+
+/**
+ * 商品类型Mapper接口
+ */
+public interface SkuClassMapper 
+{
+    /**
+     * 查询商品类型
+     * 
+     * @param classId 商品类型主键
+     * @return 商品类型
+     */
+    SkuClass selectSkuClassByClassId(Long classId);
+
+    /**
+     * 查询商品类型列表
+     * 
+     * @param skuClass 商品类型
+     * @return 商品类型集合
+     */
+    List<SkuClass> selectSkuClassList(SkuClass skuClass);
+
+    /**
+     * 新增商品类型
+     * 
+     * @param skuClass 商品类型
+     * @return 结果
+     */
+    int insertSkuClass(SkuClass skuClass);
+
+    /**
+     * 修改商品类型
+     * 
+     * @param skuClass 商品类型
+     * @return 结果
+     */
+    int updateSkuClass(SkuClass skuClass);
+
+    /**
+     * 删除商品类型
+     * 
+     * @param classId 商品类型主键
+     * @return 结果
+     */
+    int deleteSkuClassByClassId(Long classId);
+
+    /**
+     * 批量删除商品类型
+     * 
+     * @param classIds 需要删除的数据主键集合
+     * @return 结果
+     */
+    int deleteSkuClassByClassIds(Long[] classIds);
+}
---
+++ dkd-manage/src/main/java/com/dkd/manage/service/IPolicyService.java
@@ -0,0 +1,58 @@
+package com.dkd.manage.service;
+
+import java.util.List;
+import com.dkd.manage.domain.Policy;
+
+/**
+ * 策略管理Service接口
+ */
+public interface IPolicyService 
+{
+    /**
+     * 查询策略管理
+     * 
+     * @param policyId 策略管理主键
+     * @return 策略管理
+     */
+    Policy selectPolicyByPolicyId(Long policyId);
+
+    /**
+     * 查询策略管理列表
+     * 
+     * @param policy 策略管理
+     * @return 策略管理集合
+     */
+    List<Policy> selectPolicyList(Policy policy);
+
+    /**
+     * 新增策略管理
+     * 
+     * @param policy 策略管理
+     * @return 结果
+     */
+    int insertPolicy(Policy policy);
+
+    /**
+     * 修改策略管理
+     * 
+     * @param policy 策略管理
+     * @return 结果
+     */
+    int updatePolicy(Policy policy);
+
+    /**
+     * 批量删除策略管理
+     * 
+     * @param policyIds 需要删除的策略管理主键集合
+     * @return 结果
+     */
+    int deletePolicyByPolicyIds(Long[] policyIds);
+
+    /**
+     * 删除策略管理信息
+     * 
+     * @param policyId 策略管理主键
+     * @return 结果
+     */
+    int deletePolicyByPolicyId(Long policyId);
+}
